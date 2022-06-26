a04f777763d11663ba6d2427566d949ed7b68a96
Yimeng <1922799302@qq.com>
2022-06-26 23:08:49 +0800

    first commit!

---
+++ src/main/java/com/cxq/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.cxq.git;
+
+/**
+ * @author 小强
+ * @description:
+ * @belongsPackage com.cxq.git
+ * @belongsProject git-test
+ * @createTime 2022-06-26 22:59
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
